11984e5fd7ac1fe6d025cc2ab5236aefd6545d3f
onyyyyx <shadowangel404@pm.me>
2024-06-11 21:15:51 +0200

    Update

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Observations</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>M</th>
+                <th>Nom</th>
+                <th>Type</th>
+                <th>Magnitude</th>
+                <th>Ascension droite</th>
+                <th>Déclinaison</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+
+</html>
