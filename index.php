c01de4c97d6b4818e293c7021d6808ae7d33037d
onyyyyx <shadowangel404@pm.me>
2024-06-15 11:32:35 +0200

    Update

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
-<?php
+<html lang="fr">
+    <?php
+// https://hoel.jamier.free.fr
 $host = 'sql.free.fr';
 $username = 'hoel.jamier';
-$password = 'Kaqi3663';
+$password = 'Ch@tc0nf!d3nt';
 $dbname = 'hoel_jamier';
 
 // Create connection
-$conn = mysql_connect($host, $username, $password);
+$conn = new mysql_connect($host, $username, $password);
+
+
+var_dump($conn);
 
 // Check connection
 if (!$conn) {
-    die('Could not connect: ' . mysql_error());
+    die('Connection error: ' . mysql_error());
 }
 echo 'Conn: OK';
+?>
+</html>
